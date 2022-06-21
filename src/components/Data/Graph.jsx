cfa169ce147dd130749c707557c31ff3f7766aff
Mr-G-D <daredinesh@gmail.com>
2022-06-21 20:21:42 +0530

    :chart_with_upwards_trend: Line graph

--- src/components/Data/Graph.jsx
+++ src/components/Data/Graph.jsx
@@ -1,17 +1,44 @@
 import { fetchHistory } from "components/Api";
-import React, { useEffect } from "react";
+import dayjs from "dayjs";
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 
 const Graph = (props) => {
+  const [data, setData] = useState([]);
+  const [price, setPrice] = useState([]);
+
   useEffect(() => {
+    setPrice([]);
+    setData([]);
     const fetch = async () => {
       const res = await fetchHistory(props.id);
-      console.log(res);
+      res.prices.forEach((ele) => {
+        setPrice((price) => [...price, ele[1]]);
+        const date = new Date(ele[0]);
+        setData((data) => [...data, dayjs(date).format("DD/MMM")]);
+      });
     };
     fetch();
   }, [props.id]);
+
   return (
     <div id="graph" className="m-4 px-4">
-      <div className="rounded-xl bg-white"></div>
+      <div className="rounded-xl bg-white">
+        <Line
+          datasetIdKey="id"
+          data={{
+            labels: data,
+            datasets: [
+              {
+                id: 1,
+                borderColor: "red",
+                label: "",
+                data: price,
+              },
+            ],
+          }}
+        />
+      </div>
     </div>
   );
 };
